9f147b183956e725fabe0f523fe395a715ac3988
Yixuan Qiu <yixuanq@gmail.com>
2020-11-04 14:52:07 +0800

    documentation for sort_markers()

--- R/sort_markers.R
+++ R/sort_markers.R
@@ -1,6 +1,19 @@
+##' Post-processing Selected Marker Genes
+##'
+##' This function reorders the selected marker genes using information of the sample
+##' correlation matrix.
+##'
+##' @param corr    The sample correlation matrix, whose row and column names are gene names.
+##' @param markers A list of marker genes. Each component of the list is a vector of marker
+##'                gene names corresponding to a cell type. All the gene names in this list
+##'                must appear in the row/column names of \code{corr}.
 sort_markers = function(corr, markers)
 {
+    gene_names = rownames(corr)
     markers_flat = unlist(markers)
+    if(!all(markers_flat %in% gene_names))
+        stop("all gene names in 'markers' must appear in the row/column names of 'corr'")
+
     markers_ord = markers
     for(i in 1:length(markers))
     {
