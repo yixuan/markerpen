e5fbb80c9c85fd7f5afd0284f9044dc818c62bcc
Yixuan Qiu <yixuanq@gmail.com>
2021-02-28 18:51:56 +0800

    recompress data and add documentation

---
+++ R/gene_mapping.R
@@ -0,0 +1,12 @@
+#' Mapping gene names to Ensembl IDs
+#'
+#' A data set showing the mapping between gene names and Ensembl gene IDs,
+#' derived from the \pkg{EnsDb.Hsapiens.v79} Bioconductor package.
+#'
+#' @format A data frame with 59074 rows and 2 variables:
+#' \describe{
+#'   \item{ensembl}{Ensembl gene IDs}
+#'   \item{name}{corresponding gene names}
+#' }
+#' @source \url{https://bioconductor.org/packages/release/data/annotation/html/EnsDb.Hsapiens.v79.html}
+"gene_mapping"
